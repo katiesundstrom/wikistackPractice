e3057e3f0c96721ab304bb29c0a139a5182559bf
Katie Sundstrom <ksiegel@wellesley.edu>
2019-05-04 14:59:30 -0400

    sets up sequelize models and syncs with database

--- models/index.js
+++ models/index.js
@@ -2,9 +2,47 @@
 const Sequelize = require('sequelize');
 
 // instantiate Sequelize
-const db = new Sequelize('postgres://localhost:5432/wikistack');
+const db = new Sequelize('postgres://localhost:5432/wikistack', {
+  logging: false,
+});
 
-// export your db
+// require models
+
+const User = db.define('user', {
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  email: {
+    type: Sequelize.STRING,
+    isEmail: true,
+    allowNull: false,
+  },
+});
+
+const Page = db.define('page', {
+  title: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  slug: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    //since we are searching, editing, deleting by slug, these need to be unique
+    unique: true,
+  },
+  content: {
+    type: Sequelize.TEXT,
+    allowNull: false,
+  },
+  status: {
+    type: Sequelize.ENUM('open', 'closed'),
+  },
+});
+
+// export your db and models
 module.exports = {
   db,
+  User,
+  Page,
 };
